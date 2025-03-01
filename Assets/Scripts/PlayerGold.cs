e9dfa9cc6ac221b14057561d01320932d01c48bb
user1 <user1@user1ui-MacBookPro.local>
2025-03-01 13:00:47 +0900

    first commit

---
+++ Assets/Scripts/PlayerGold.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerGold : MonoBehaviour
+{
+	[SerializeField]
+	private	int	currentGold = 1000;
+
+	public int CurrentGold
+	{
+		set => currentGold = Mathf.Max(0, value+100);
+		get => currentGold;
+	}
+}
+
+
+/*
+ * File : PlayerGold.cs
+ * Desc
+ *	: 플레이어의 소지 골드 정보
+ *	
+ */
